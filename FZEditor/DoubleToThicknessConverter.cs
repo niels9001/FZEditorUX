a04f197d999edd4e088cc719bc295d58aa203f6b
Niels Laute <niels9001@hotmail.com>
2021-09-27 20:17:02 +0200

    Updates

---
+++ FZEditor/DoubleToThicknessConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace FZEditor
+{
+
+    public class DoubleToThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+        object parameter, string language)
+        {
+            var var = (double)value;
+
+            return new Thickness(var, var, var, var);
+        }
+
+        public object ConvertBack(object value, Type targetType,
+        object parameter, string language)
+        {
+            return value;
+        }
+    }
+}
